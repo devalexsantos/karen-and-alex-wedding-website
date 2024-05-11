6658b1a962f1138b8ba26b14cb91c64a9b004e82
Alex Santos <devalexsantos@gmail.com>
2024-05-11 11:11:24 -0300

    first commit: setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#BD945A"
+      },
+      fontFamily: {
+        head: ['var(--font-alex-brush)'],
+        base: ['var(--font-nunito-sans)'],
+      },
     },
   },
   plugins: [],
